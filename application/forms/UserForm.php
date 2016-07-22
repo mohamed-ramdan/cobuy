fa048cbb42989c95b1a6ffaab0eae3d6cf79f289
mohamed-ramdan <mramadan181291@gmail.com>
2016-07-22 06:43:36 +0200

    add 3 elements to user form for coashing

--- application/forms/UserForm.php
+++ application/forms/UserForm.php
@@ -6,6 +6,40 @@ class Application_Form_UserForm extends Zend_Form
     public function init()
     {
         /* Form Elements & Other Definitions Here ... */
+        $firstName = new Zend_Form_Element_Text("firstName");
+        $firstName->setLabel('First Name: ');
+        $firstName->setRequired();
+        
+        // this line class="form-element" is just CSS design.
+        $firstName->setAttrib("class", "form-element");
+        
+        
+        // Define other elements here with the same way
+        
+        
+        
+        
+        $submit = new Zend_Form_Element_Submit("submit");
+        $submit->setValue("Save");
+        $submit->setAttrib("class", "btn btn-success");
+        
+        
+        $reset = new Zend_Form_Element_Reset("reset");
+        $reset->setValue("Cancel");
+        $reset->setAttrib("class", "btn btn-danger");
+        
+        
+        
+        
+        // add form elements 
+        $this->addElements(array(
+            $firstName,
+            //other elements here ...
+            $submit,
+            $reset
+        ));
+        
+        
     }
 
 
